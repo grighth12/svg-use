381dba4f65fa37c622e29672d686b08cd33a268a
grighth12 <grighth12@gmail.com>
2024-03-02 13:22:37 +0900

    svgr 관련 코드 삭제, use만 남김

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import reactLogo from "./assets/react.svg";
-import ViteLogo from "/vite.svg?react";
+// import ViteLogo from "/vite.svg?react";
 import "./App.css";
-// import { ReactIcon } from "./assets/react";
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-
-        <ViteLogo />
+        {/* <ViteLogo /> */}
 
         <svg>
           {/* TODO: xlink를 넣어본다. */}
